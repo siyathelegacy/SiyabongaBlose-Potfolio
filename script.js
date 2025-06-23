ef744d58ca68979fd2b8f3f83d02ab5e53821793
siyathelegacy <70043366+siyathelegacy@users.noreply.github.com>
2025-06-23 13:15:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const navbarToggle =document.querySelector('.navbar-toggle');
+
+const navbarMenu = document.querySelector('.navbar-menu');
+
+navbarToggle.addEventListener('click',()=>{
+    navbarToggle.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+})
+
 function addRecommendation() {
   // Get the message of the new recommendation
   let recommendation = document.getElementById("new_recommendation");
@@ -9,7 +18,8 @@ function addRecommendation() {
     // Create a new 'recommendation' element and set it's value to the user's message
     var element = document.createElement("div");
     element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
+    element.innerHTML = "\<span\>&#8220;\</span\>" +
+    recommendation.value + "\<span\>&#8221;\</span\>";
     // Add this element to the end of the list of recommendations
     document.getElementById("all_recommendations").appendChild(element); 
     
@@ -17,7 +27,6 @@ function addRecommendation() {
     recommendation.value = "";
   }
 }
-
 function showPopup(bool) {
   if (bool) {
     document.getElementById('popup').style.visibility = 'visible'
@@ -25,3 +34,7 @@ function showPopup(bool) {
     document.getElementById('popup').style.visibility = 'hidden'
   }
 }
+/*scrolling menu button*/ 
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
